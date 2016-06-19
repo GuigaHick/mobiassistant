d1bb21ac7ba0c55edfcacc63f5474379981304a3
Guilherme <ghss@cin.ufpe.br>
2016-06-19 17:03:31 -0300

    organizing folder according by new aproach

--- MobiAssistant/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('cdg', [require('angular-route'),'angularUtils.directives.dirPagination']);
-
-app.config(function($routeProvider){
-	$routeProvider.when("/pessoas", {
-		templateUrl : "views/bus.html",
-		controller : "busController",
-        access: { requiredLogin: false }
-	});
-});
--- MobiAssistant/services/dbService.js
+++
@@ -1,7 +0,0 @@
-"USE STRICT";
-app.factory("dbService", function($http){
-	var sqlite = require('sqlite-sync');
-	var db = sqlite.connect('model/database_mobiassistant.db');
-	//var db = sqlite.connect('model/database.db');
-	return db;
-});
